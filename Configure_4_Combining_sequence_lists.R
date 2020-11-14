f69a6432eeef3ad466556ab999bf6314c8dd50f3
Lars Nauheimer <68572416+LarsNauheimer@users.noreply.github.com>
2020-11-14 16:49:59 +1000

    Update Configure_4_Combining_sequence_lists.R

--- Configure_4_Combining_sequence_lists.R
+++ Configure_4_Combining_sequence_lists.R
@@ -21,19 +21,29 @@ contig = ""
 # name the output sequence list folder 
 name_of_sequence_list_output = ""
 
-# Sample list
-# The default is to use the sample lists used in previous scripts, one for the normal and one for the phased reads. 
-# They will be merged and all samples that appear in both lists as well as are available in the sequence lists will be used. 
-# From that list samples can be removed by listing them in text file (samples to exclude).
-#
-# The other option is to provide a text file with included samples and thus overwriting the default. 
-#
-
 # set nameslist file of original accessions (e.g. "~/hybpiper/namelist.txt")
 txt_file_with_list_of_accessions = ""
 
 # set nameslist file of phased accessions (e.g. "~/hybpiper_phased/namelist_phased.txt")
 txt_file_with_list_of_phased_accessions = ""
+
+# select whether the phased accessions should be used instead of the non phased accessions of a sample. If "no" is chosen the phased and not phased accessions of the same sample are included. 
+exchange_phased_with_not_phased_samples = "yes"   # "yes" or "no" 
+
+# finally, sometimes it can be that there are phased loci included that were excluded in the non-phased list. In case they should be included, set the next variable to "yes"
+include_phased_seqlists_when_non_phased_locus_absent = "no"  # "yes" or "no"
+
+
+# Optional: adjust list of samples and loci 
+#
+# The default is to use the sample lists used in previous scripts, one for the normal and one for the phased reads. 
+# They will be merged and all samples that appear in both lists will be used. 
+# Samples can be removed by listing them in text file (file_with_samples_excluded).
+#
+# Or one can provide a list only with the included samples (file_with_samples_included).
+# The other option is to provide a text file with included samples and thus overwriting the default. 
+#
+# The same can be done for loci.
 
 # select samples to exclude  (one sample per row in a text file).  "" will exclude none.
 file_with_samples_excluded = ""
@@ -49,11 +59,4 @@ file_with_loci_excluded = ""
 
 # select loci to include (text file with one locus per row). "" will include all.
 file_with_loci_included = ""
-
-# select whether the phased accessions should be used instead of the non phased accessions of a sample. If "no" is chosen the phased and not phased accessions of the same sample are included. 
-exchange_phased_with_not_phased_samples = "yes"   # "yes" or "no" 
-
-# finally, sometimes it can be that there are phased loci included that were excluded in the non-phased list. In case they should be included, set the next variable to "yes"
-include_phased_seqlists_when_non_phased_locus_absent = "no"  # "yes" or "no"
-
 
