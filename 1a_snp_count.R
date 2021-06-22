7aae3ac9e8ebe276dad4a4f755c8a7cd394a5c0d
Lars Nauheimer <68572416+LarsNauheimer@users.noreply.github.com>
2021-06-23 08:58:27 +1000

    Add files via upload

---
+++ 1a_snp_count.R
@@ -0,0 +1,98 @@
+##################
+### SNPs count ###
+##################
+
+# load config
+if (!(exists("config_file"))) {config_file <- "./config.txt"}
+source(config_file)
+
+# load packages
+library(ape)
+library(seqinr)
+library(stringr)
+
+# generate folder
+output_Robjects <- file.path(path_to_output_folder,"00_R_objects",name_for_particular_dataset_optimization_subset)
+dir.create(output_Robjects, showWarnings = F)
+
+#####################################################################################
+### Counting polymorphic sites (masked as ambiguity codes in conseneus sequences) ###
+#####################################################################################
+
+# getting targets and sample names
+if(targets_file_format == "AA"){
+  targets <- read.fasta(fasta_file_with_targets, seqtype = "AA", as.string = TRUE, set.attributes = FALSE)
+  } else if(targets_file_format == "DNA"){
+    targets <- read.fasta(fasta_file_with_targets, seqtype = "DNA", as.string = TRUE, set.attributes = FALSE)
+  } else {
+    print("Warning! Target file type not set properly. Should be 'DNA' or 'AA'!")
+}
+targets_name <- unique(gsub(".*-","",labels(targets)))
+
+samples <- readLines(path_to_namelist)
+
+
+
+if(intronerated_contig=="yes"){
+  intronerated_name <- "intronerated" 
+  intronerated_underscore <- "_"
+} else {
+  intronerated_name <- ""
+  intronerated_underscore <- ""
+  }
+
+# function for counting ambiguities
+seq_stats <- function(file){
+  fasta <- read.fasta(file, as.string=TRUE, set.attributes = FALSE )
+  seq <- gsub("N|[?]|-","",fasta[[1]])
+  c(round(str_length(seq),0),(str_count(seq,"Y|K|R|S|M|y|k|r|s|m|w") + str_count(seq,"W|D|H|B|V|w|d|h|b|v")*2) )
+}
+
+# generate empty tables for SNPS and sequence length
+tab_snps <- data.frame(loci=targets_name)
+tab_length <- data.frame(loci=targets_name)
+
+# fill tables with information on snps and sequence length for each sample and locus 
+start_time <- Sys.time()
+for(sample in samples){
+  #sample <- samples[1]
+  print(paste(sample," ", round(difftime(Sys.time(),start_time, units="secs"),0),"s", sep="") )
+  
+  tab_sample <- data.frame(targets=targets_name, seq_length=NA, ambis=NA, ambi_prop=NA)
+  
+  
+  consensus_files <- list.files(file.path(path_to_output_folder,"01_data",sample, paste(intronerated_name,intronerated_underscore,"consensus", sep="")),full.names = T)
+  
+  for(consensus_file in consensus_files) {
+    
+    gene <- gsub("(_intronerated|).fasta","",gsub(".*/","",consensus_file))
+    
+    file.path(path_to_output_folder,"01_data",sample, "consensus", paste(gene,intronerated_underscore,intronerated_name,".fasta",sep=""))
+    
+    if(file.info(consensus_file)$size !=0){
+      stats <- seq_stats(consensus_file)
+    } else {
+      stats <- c(NA,NA)
+    }  
+    tab_sample$seq_length[grep(gene,tab_sample$targets)] <- stats[1]
+    tab_sample$ambis[grep(gene,tab_sample$targets)] <- stats[2]  
+  
+  }
+  
+  tab_sample$ambi_prop <- tab_sample$ambis/tab_sample$seq_length  
+  tab_snps[grep(sample,samples)] <- tab_sample$ambi_prop
+  tab_length[grep(sample,samples)] <- tab_sample$seq_length
+
+}
+
+
+colnames(tab_snps) <- samples
+rownames(tab_snps) <- targets_name
+colnames(tab_length) <- samples
+rownames(tab_length) <- targets_name
+
+### Generate output tables and save data in Robjects
+
+
+saveRDS(tab_snps,file=file.path(output_Robjects,"Table_SNPs.Rds"))
+saveRDS(tab_length,file=file.path(output_Robjects,"Table_consensus_length.Rds"))
---
+++ 2b_collate_bbsplit_results.R
@@ -0,0 +1,81 @@
+##############################################################
+### Collection of info in log files from clade association ###
+##############################################################
+
+# load config
+if (!(exists("config_file"))) {config_file <- "./config.txt"}
+source(config_file)
+
+# get names of stats files and from it get the name of samples 
+folder_bbsplit_stats <- file.path(path_to_clade_association_folder,"bbsplit_stats")
+stats_files <- list.files(folder_bbsplit_stats, "*_bbsplit-stats.txt")
+sample <- gsub("(_mapped.*|.fasta.*|_bbsplit-stats.*)","",stats_files)  # this removes everything after the first . in the name
+
+ref_samples <- read.csv(csv_file_with_clade_reference_names, header = T)
+colnames(ref_samples)[1] <- "sample"
+colnames(ref_samples)[2] <- "abbreviation"
+
+#set reference names (full names or abbreviations)
+if("abbreviation" %in% colnames(ref_samples)){
+  ref_names <- ref_samples$abbreviation
+} else {
+  ref_names <- ref_samples$sample
+}
+
+
+# generate empty table (ref_names x samples)
+tab_clade_assoc <- matrix(nrow = length(ref_names), ncol = length(sample))
+rownames(tab_clade_assoc) <- ref_names
+colnames(tab_clade_assoc) <- sample
+
+
+for(i in 1:length(stats_files)){
+  #i=1
+  stats_file <- stats_files[i]
+  
+  p_unamb <- read.delim(file.path(folder_bbsplit_stats, stats_file))[,1:2]
+  
+  tab_clade_assoc[,i] <- p_unamb$X.unambiguousReads[match(rownames(tab_clade_assoc), p_unamb$X.name)]
+  
+}
+
+
+# The proportions of reads matching unambigously to a reference are sensitive to the genetic distance of references. 
+# E.g. two clade references that are closer related to each other have lower proportions of reads matching to the reference because more reads match ambiguously between those references. 
+# One way to get more comparable data is normalization of proportions, by dividing the proportion of reads matched to a reference by the results from the sample that was used as reference. 
+# This assumes that the reads of the reference sequence matching to the same reference is the maximum possible and therefore set to 1. However, this can deviate due to differnces in sequencing quality, read coverage and other factors. 
+
+
+tab_clade_assoc_norm <- tab_clade_assoc
+
+for(i in 1:length(rownames(tab_clade_assoc))){
+  if(length(grep(ref_samples$sample[i],colnames(tab_clade_assoc)))>0){
+    tab_clade_assoc_norm[i,] <- tab_clade_assoc[i,] / tab_clade_assoc[i,grep(ref_samples$sample[i],colnames(tab_clade_assoc))]
+  }
+}
+
+
+ttab_clade_assoc <- as.data.frame(t(tab_clade_assoc))
+ttab_clade_assoc$sample <- rownames(ttab_clade_assoc)
+
+ttab_clade_assoc_norm <- as.data.frame(t(tab_clade_assoc_norm))
+ttab_clade_assoc_norm$sample <- rownames(ttab_clade_assoc_norm)
+
+
+# for better assessment, the summary table is added to the clade assessment results
+
+summary_het_ad_clean <- readRDS(file.path(path_to_output_folder,"00_R_objects", name_of_subset_to_use_in_summary_table, "Summary_table.Rds"))
+
+tab_hetad_cladeasso <- merge(summary_het_ad_clean,ttab_clade_assoc, by="sample", incomparables = F)
+
+tab_hetad_cladeasso_norm <- merge(summary_het_ad_clean,ttab_clade_assoc_norm, by="sample")
+
+# save output
+write.csv(t(tab_clade_assoc), file=file.path(path_to_clade_association_folder,"Table_clade_association.csv"))
+write.csv(t(tab_clade_assoc_norm), file=file.path(path_to_clade_association_folder,"Table_clade_association_normalised.csv"))
+write.csv(tab_hetad_cladeasso, file=file.path(path_to_clade_association_folder,"Table_clade_association_and_summary.csv"))
+write.csv(tab_hetad_cladeasso_norm, file=file.path(path_to_clade_association_folder,"Table_clade_association_and_summary_normalised.csv"))
+
+saveRDS(tab_clade_assoc, file.path(path_to_clade_association_folder,"Table_clade_association.RDS"))
+saveRDS(tab_clade_assoc_norm, file.path(path_to_clade_association_folder,"Table_clade_association_norm.RDS"))
+
