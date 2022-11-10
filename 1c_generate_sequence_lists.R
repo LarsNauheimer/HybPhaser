05bbbae8cd3896265f311a77dafe5049cd96fb8e
bmichanderson <banderson2914@gmail.com>
2022-11-10 16:51:19 +0800

    make search pattern unique per locus

--- 1c_generate_sequence_lists.R
+++ 1c_generate_sequence_lists.R
@@ -123,7 +123,7 @@ if(Sys.info()['sysname']=="Linux"){
   } else {
     for(locus in targets_name){
       #list all fasta files from that locus for all samples
-      fasta_files <- list.files(path=file.path(path_to_output_folder,"01_data/"), pattern=paste(locus,".fasta",sep=""), recursive=TRUE, full.names = TRUE)
+      fasta_files <- list.files(path=file.path(path_to_output_folder,"01_data/"), pattern=paste("^",locus,".fasta",sep=""), recursive=TRUE, full.names = TRUE)
       #select consensus/contig files
       consensus_files <- grep("consensus",fasta_files,value = TRUE)
       contigs_files <- grep("contigs",fasta_files,value = TRUE)
